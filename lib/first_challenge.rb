020e6ef19df589859d842c012bd98281aa75dbb7
fhernandez173 <f.hernandez173@gmail.com>
2015-11-23 16:32:23 -0500

    done

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -13,10 +15,25 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+  # iterate over hash contacts
+  contacts.each do |key, value|
+    # interate through the values (Jon, Freddy)
+    value.each do |the_key, the_value|
+      # condition to check if The_value is an array and contains strawberry
+      if the_value.class == Array && the_value.include?("strawberry")
+        the_value.delete "strawberry"
+      end
+     end
+   # end
+    #remove strawberry
+    # if key == "Jon Snow"
+    #   value.delete_if do |the_key, the_value|
+    #     the_value == "strawberry"
+    #   end
+    # end
+  end
   #remember to return your newly altered contacts hash!
   contacts
+  #binding.pry
 end
 
